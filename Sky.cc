ca4da0350368f27db960cd9c5c3b34fd026736ab
Sky <2503931698@qq.com>
2026-03-04 16:08:31 +0800

    Update Sky.cc

--- Sky.cc
+++ Sky.cc
@@ -8,7 +8,6 @@ game:GetService("StarterGui"):SetCore("SendNotification", {
   Title = "正在绕过反作弊ing",
   Text = "加载卡顿 请多多包容",
 })
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Pixeluted/adoniscries/main/Source.lua", true))()
 wait(1.5)
 game:GetService("StarterGui"):SetCore("SendNotification", {
   Title = "加载成功！",
