4eccfd5a8619a624087768e5308ced909f5ec19d
Sky <2503931698@qq.com>
2026-03-04 16:00:39 +0800

    Create Sky.cc

---
+++ Sky.cc
@@ -0,0 +1,17 @@
+--好吧 整蛊失败
+game:GetService("StarterGui"):SetCore("SendNotification", {
+  Title = "犯罪最强脚本Sky.cc",
+  Text = "由Sky制作 永久免费",
+})
+wait(1.5)
+game:GetService("StarterGui"):SetCore("SendNotification", {
+  Title = "正在绕过反作弊ing",
+  Text = "加载卡顿 请多多包容",
+})
+wait(1.5)
+game:GetService("StarterGui"):SetCore("SendNotification", {
+  Title = "加载成功！",
+  Text = "感谢使用",
+})
+wait(2)
+game.Players.LocalPlayer:Kick("想屁吃呢")
